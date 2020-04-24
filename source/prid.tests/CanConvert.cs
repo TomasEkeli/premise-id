1fd96a618882442e0ce9340a1dcdc27c759848ae
Tomas Ekeli <tomas.ekeli@gmail.com>
2020-04-24 16:59:25 +0200

    convert and deconvert prids

--- source/prid.tests/CanConvert.cs
+++ source/prid.tests/CanConvert.cs
@@ -1,14 +1,15 @@
 ﻿using Xunit;
 using FluentAssertions;
+using System;
 
 namespace premise_id.tests
 {
-    public class CanConvert
+    public class PridTests
     {
         [Fact]
         public void When_checking_a_string_with_unsupported_characters()
         {
-            var unsupported = "æøå";
+            const string unsupported = "æøå";
 
             prid.Can_convert_all_characters_in(unsupported)
                 .Should()
@@ -18,9 +19,9 @@ namespace premise_id.tests
         [Fact]
         public void When_checking_a_string_with_supported_characters()
         {
-            var unsupported = "stuv";
+            const string supported = "stuv";
 
-            prid.Can_convert_all_characters_in(unsupported)
+            prid.Can_convert_all_characters_in(supported)
                 .Should()
                 .Be(true);
         }
@@ -28,9 +29,9 @@ namespace premise_id.tests
         [Fact]
         public void When_checking_a_string_with_uppercase_supported_characters()
         {
-            var unsupported = "STUV";
+            const string supported = "STUV";
 
-            prid.Can_convert_all_characters_in(unsupported)
+            prid.Can_convert_all_characters_in(supported)
                 .Should()
                 .Be(true);
         }
@@ -38,11 +39,90 @@ namespace premise_id.tests
         [Fact]
         public void When_checking_a_string_with_characters_allowed_in_guid()
         {
-            var unsupported = "0987654321abcdef";
+            const string supported = "0987654321abcdef";
 
-            prid.Can_convert_all_characters_in(unsupported)
+            prid.Can_convert_all_characters_in(supported)
                 .Should()
                 .Be(true);
+        }
+
+        [Fact]
+        public void When_converting_a_too_long_string()
+        {
+            const string too_long = "abcabcabacbacbdaccbaadbadcadbadcbacaadadcbcadbcadcbacd";
+
+            var exception = Record.Exception(
+                () => _ = prid.Convert(too_long)
+            );
+
+            exception
+                .Should()
+                .BeOfType<ValueTooLongToFitInGuid>();
+
+            exception
+                .Message
+                .Should()
+                .Be(
+                    $"The value '{too_long}' cannot fit in a GUID, it is too large."
+                );
+        }
+
+        [Fact]
+        public void When_converting_a_string_with_unsupported_characters()
+        {
+            const string unsupported = "kamelåså";
+
+            var exception = Record.Exception(
+                () => _ = prid.Convert(unsupported)
+            );
+
+            exception
+                .Should()
+                .BeOfType<ValueContainsUnsupportedCharacters>();
+
+            exception
+                .Message
+                .Should()
+                .Be(
+                    $"The value '{unsupported}' contains unsupported characters."
+                );
+        }
+
+        [Theory]
+        [InlineData("This is fun!", "741515f6117000000000000000000000")]
+        [InlineData("I can convert sentences to IDs.", "1ca17c017aae1275e177e17ce5701d50")]
+        [InlineData("Not too long sentences.", "17077002-0176-5e17-7e17-ce5000000000")]
+        [InlineData("What use is this?", "aaa4a761-5e15-7415-0000-000000000000")]
+        [InlineData("It is just entertaining.", "17153615-7e17-7e12-7a11-711760000000")]
+        public void When_converting_a_string_with_supported_characters(
+            string input,
+            string expected
+        )
+        {
+            Guid the_expected_guid = new Guid(expected);
+
+            Guid result = prid.Convert(input);
+
+            result
+                .Should()
+                .Be(the_expected_guid);
+        }
+
+        [Theory]
+        [InlineData("thisisfun", "741515f6117000000000000000000000")]
+        [InlineData("kanconvertsentencestoids", "1ca17c017aae1275e177e17ce5701d50")]
+        [InlineData("nottoolongsentences", "17077002-0176-5e17-7e17-ce5000000000")]
+        [InlineData("whatuseisthis", "aaa4a761-5e15-7415-0000-000000000000")]
+        [InlineData("nisjustentertaining", "17153615-7e17-7e12-7a11-711760000000")]
+        public void When_reifying_a_guid_to_a_string(string expected, string input)
+        {
+            Guid the_prid = new Guid(input);
+
+            var result = prid.Deconvert(the_prid);
+
+            result
+                .Should()
+                .Be(expected);
         }
     }
 }
--- source/prid/prid.cs
+++ source/prid/prid.cs
@@ -1,49 +1,164 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace premise_id
 {
     public static class prid
     {
-        public static string legal_guid_characters = "0123456789abcdef";
-
-        public static IEnumerable<(char, string)> mappings = new []
-        {
-            ('g', "6"),
-            ('h', "4"),
-            ('i', "1"),
-            ('k', "1c"),
-            ('l', "2"),
-            ('m', "a4"),
-            ('n', "17"),
-            ('o', "0"),
-            ('p', "9"),
-            ('r', "12"),
-            ('s', "5"),
-            ('t', "7"),
-            ('u', "61"),
-            ('v', "aa"),
-            ('w', "aaa"),
-            ('x', "8"),
-            ('y', "14"),
-            (' ', ""),
-            ('-', ""),
-            (',', ""),
-            ('.', ""),
-            (':', ""),
-        };
+        public static IDictionary<char, string> mappings =
+            new Dictionary<char, string>()
+            {
+                {'0', "0"},
+                {'1', "1"},
+                {'2', "2"},
+                {'3', "3"},
+                {'4', "4"},
+                {'5', "5"},
+                {'6', "6"},
+                {'7', "7"},
+                {'8', "8"},
+                {'9', "9"},
+                {'a', "a"},
+                {'b', "b"},
+                {'c', "c"},
+                {'d', "d"},
+                {'e', "e"},
+                {'f', "f"},
+                {'g', "6"},
+                {'h', "4"},
+                {'i', "1"},
+                {'j', "3"},
+                {'k', "1c"},
+                {'l', "2"},
+                {'m', "a4"},
+                {'n', "17"},
+                {'o', "0"},
+                {'p', "9"},
+                {'r', "12"},
+                {'s', "5"},
+                {'t', "7"},
+                {'u', "61"},
+                {'v', "aa"},
+                {'w', "aaa"},
+                {'x', "8"},
+                {'y', "14"},
+                {' ', ""},
+                {'-', ""},
+                {',', ""},
+                {'.', ""},
+                {':', ""},
+                {'!', ""},
+                {'?', ""},
+            };
 
         public static bool Can_convert_all_characters_in(string candidate)
         {
-            var allowed_characters = mappings.Select(_ => _.Item1);
-
             return candidate
                 .ToLowerInvariant()
                 .ToCharArray()
-                .All(_ =>
-                    allowed_characters.Contains(_)
-                    || legal_guid_characters.Contains(_)
+                .All(_ => mappings.ContainsKey(_));
+        }
+
+        public static Guid Convert(string value)
+        {
+            if (!Can_convert_all_characters_in(value))
+            {
+                throw new ValueContainsUnsupportedCharacters(value);
+            }
+            var as_legal_characters = convert_to_legal(value);
+
+            if (as_legal_characters.Length > 31)
+            {
+                throw new ValueTooLongToFitInGuid(value);
+            }
+
+            return new Guid(as_legal_characters.PadRight(32, '0'));
+        }
+
+        public static string Deconvert(Guid prid)
+        {
+            var as_string = prid
+                .ToString()
+                .ToLowerInvariant()
+                .Replace("-", "");
+
+            var reverse_mapping = mappings
+                .Skip(10)
+                .ToList()
+                .OrderByDescending(_ => _.Value.Length);
+
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < as_string.Length; i++)
+            {
+                if (as_string[i] == 'a'
+                    && i + 2 < as_string.Length
+                    && as_string[i + 1] == 'a'
+                    && as_string[i + 2] == 'a')
+                {
+                    sb.Append("w");
+                    i += 2;
+                    continue;
+                }
+
+                if (i + 1 < as_string.Length
+                    && reverse_mapping
+                        .Any(_ =>
+                            _.Value == as_string.Substring(i, 2)
+                        )
+                )
+                {
+                    sb.Append(
+                        reverse_mapping
+                            .First(_ =>
+                                _.Value == as_string.Substring(i, 2)
+                            )
+                            .Key
+                    );
+
+                    i++;
+                    continue;
+                }
+
+                sb.Append(
+                    reverse_mapping
+                        .First(_ => _.Value == $"{as_string[i]}")
+                        .Key
                 );
+            }
+
+            return sb.ToString().TrimEnd('o');
+        }
+
+        static string convert_to_legal(string value)
+        {
+            return string
+                .Concat(
+                    value
+                        .ToLowerInvariant()
+                        .ToArray()
+                        .Select(_ => mappings[_])
+                );
+        }
+    }
+
+    [Serializable]
+    public class ValueContainsUnsupportedCharacters : Exception
+    {
+        public ValueContainsUnsupportedCharacters(string value)
+        : base($"The value '{value}' contains unsupported characters.")
+        {
+        }
+    }
+
+    [Serializable]
+    public class ValueTooLongToFitInGuid : Exception
+    {
+        public ValueTooLongToFitInGuid(string value)
+        : base($"The value '{value}' cannot fit in a GUID, it is too large.")
+        {
         }
     }
 }
